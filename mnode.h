a023c99e4a22f797ca49e937f5a343808ed219f6
Arya <83908779+aryad-16@users.noreply.github.com>
2023-04-28 23:32:22 +0530

    mc

--- mnode.h
+++ mnode.h
@@ -17,7 +17,7 @@ void insert(int value, int *depth, Mnode tree[]);
 int randLeaf(int depth);
 int val(Mnode head);
 int findInsertionPoint(int value, int *depth, Mnode tree[]);
-int extractMin(Mnode tree[], int);
+int extractMin(Mnode tree[], int depth);
 void moundify(Mnode tree[], int index, int depth);
 void swap(Mnode tree[], int l, int r);
 #endif
