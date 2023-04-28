08c0c79066b6970b14b05a58fb98c923b8b42789
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2023-04-28 23:06:10 +0530

    extract min done

--- mnode.c
+++ mnode.c
@@ -28,7 +28,7 @@ void printList(Mnode head)
 void moundify(Mnode tree[], int index, const int depth)
 {
     // if the index is a leaf node, it is not dirty by default , hence return
-    if (index >= pow(2, depth) - 1 || index <= pow(2, depth) - 1)
+    if (index >= pow(2, depth - 1) - 1 || index <= pow(2, depth) - 2)
     {
         tree[index].dirty = false;
         return;
@@ -38,23 +38,46 @@ void moundify(Mnode tree[], int index, const int depth)
     int l = 2 * index + 1;
     int r = 2 * index + 2;
 
-    // if the index is not a leaf node, moundify its children
-    // the Mnode with the least value is swapped with the parent node
+    /* if the index is not a leaf node, moundify its children
+     the Mnode with the least value is swapped with the parent node
+     and moundify is called on the swapped node */
+
+    // child l <= child r
     if ((tree[l].list->value <= tree[r].list->value) && (tree[l].list->value < tree[index].list->value))
     {
         swap(tree, l, r);
         moundify(tree, l, depth);
     }
+    // child l > child r
     else if ((tree[l].list->value > tree[r].list->value) && (tree[r].list->value < tree[index].list->value))
     {
         swap(tree, l, r);
         moundify(tree, r, depth);
     }
+    // properly moundified
     else
     {
         tree[index].dirty = false;
         return;
     }
+}
+
+int extractMin(Mnode tree[], int depth)
+{
+    if (tree[0].list->value == INT_MAX)
+    {
+        return INT_MAX;
+    }
+    // minimum value is stored in the root node's list first element
+    int result = tree[0].list->value;
+    Lnode *temp = tree[0].list;
+    tree[0].list = tree[0].list->next;
+    free(temp);
+
+    // moundify the tree, starting from the root node
+    moundify(tree, 0, depth);
+
+    return result;
 }
 
 // function for swapping any two nodes
--- mnode.h
+++ mnode.h
@@ -16,7 +16,7 @@ void insertAtBeginning(Mnode head, int value);
 void printList(Mnode head);
 
 void insert(Mnode tree[], int value);
-int extractMin(Mnode tree[]);
+int extractMin(Mnode tree[], int depth);
 void moundify(Mnode tree[], int index, int depth);
 void swap(Mnode tree[], int l, int r);
 #endif
